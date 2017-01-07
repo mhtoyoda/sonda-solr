8749b38cb5a7d4e02bb0244f4d385e18996c57c0
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-01-07 12:16:26 -0200

    Estrutura inicial

---
+++ src/main/java/br/com/toyoda/elo7/SondaSolrApplication.java
@@ -0,0 +1,12 @@
+package br.com.toyoda.elo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondaSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondaSolrApplication.class, args);
+	}
+}
